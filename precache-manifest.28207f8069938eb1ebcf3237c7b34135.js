cfdb9553335baa0d43d145258605c3c969308a75
Khalid <khalid.mgaad@gmail.com>
2020-03-05 00:32:54 +0100

    Updates

---
+++ precache-manifest.28207f8069938eb1ebcf3237c7b34135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b636827b1559d2a67773e52f6428e999",
+    "url": "/Currency-Converter-ReactJS/index.html"
+  },
+  {
+    "revision": "8531f8909d9454f0dd81",
+    "url": "/Currency-Converter-ReactJS/static/css/main.90447837.chunk.css"
+  },
+  {
+    "revision": "ce59bca4a92d2fbdb3da",
+    "url": "/Currency-Converter-ReactJS/static/js/2.5406572b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Currency-Converter-ReactJS/static/js/2.5406572b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8531f8909d9454f0dd81",
+    "url": "/Currency-Converter-ReactJS/static/js/main.987d189b.chunk.js"
+  },
+  {
+    "revision": "962470d724db7f9f2d69",
+    "url": "/Currency-Converter-ReactJS/static/js/runtime-main.80bc3466.js"
+  }
+]);
